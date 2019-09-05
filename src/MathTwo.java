56561840c8aec49a94a468a9593d3c806d3bda86
jkneisler1 <John.Kneisler@gmail.com>
2019-09-05 10:01:25 -0400

    Math Two program

---
+++ src/MathTwo.java
@@ -0,0 +1,7 @@
+public class MathTwo {
+    public static void main(String[] args) {
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println("Mynumber is " + mynumber + ", and myothernumber is: " + myothernumber);
+    }
+}
